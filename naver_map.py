ac318440321ade2782ce6637fb8616ee0c81af3d
sohee0 <108312161+sohee0@users.noreply.github.com>
2022-10-27 17:41:26 +0900

    20221027

--- naver_map.py
+++ naver_map.py
@@ -1,3 +1,4 @@
+from msilib.schema import Error
 import pandas as pd
 import numpy as np
 import time
@@ -37,7 +38,7 @@ def getNavermap(result):
 
     for i, keyword in enumerate(df['명칭'].tolist()): 
     
-        print("이번에 찾을 키워드 :", i, f"/ {df.shape[0]} 행", keyword) 
+        print('관광지 :', i, f"/ {df.shape[0]}", keyword) 
     
         try: 
             wd.get(f'https://map.naver.com/v5/search/{keyword}=14377346.8495072,4186044.6333144,15,0,0,0,dh') # 검색 url 접속 = 검색하기 
@@ -50,7 +51,7 @@ def getNavermap(result):
                 
         
             wd.switch_to.frame('entryIframe')
-            wd.find_element(By.XPATH,'//*[@id="app-root"]/div/div/div/div[2]/div[1]').click()
+            wd.find_element(By.XPATH,'//*[@id="app-root"]/div/div/div/div[2]/div[1]')
             time.sleep(3)
             html = wd.page_source
             soup = BeautifulSoup(html, 'html.parser')
@@ -60,45 +61,37 @@ def getNavermap(result):
                 print(item.get_text())
 
             result.append([review])
+        
         except:
-            wd.switch_to.frame('searchIframe')
-            # wd.find_element(By.XPATH,'//*[@id="_pcmap_list_scroll_container"]/ul/li[1]').click()
-            wd.find_element(By.XPATH,'//*[@id="_pcmap_list_scroll_container"]/ul/li[1]/div[2]/a[1]/div/div/span[1]').click()
-            time.sleep(4)
-            html = wd.page_source
-            soup = BeautifulSoup(html, 'html.parser')  
-            # span 태그에 class 속성 사용 추출하기
-            review = soup.find_all('span', attrs={'class':'PXMot'})
-            
-            for item in review:
-                print(item.get_text())
+            try:
+                wd.switch_to.frame('searchIframe')
+                wd.find_element(By.XPATH,'//*[@id="_pcmap_list_scroll_container"]/ul/li[1]/div[2]/a[1]/div/div/span[1]').click()
+                time.sleep(3)
+                # 새로운 프레임으로 들어가기위해서 먼저 빠져 나와야함
+                wd.switch_to.default_content()
+                wd.switch_to.frame('entryIframe')
+                wd.find_element(By.XPATH,'//*[@id="app-root"]/div/div/div/div[2]/div[1]')
+                time.sleep(3)
+                html = wd.page_source
+                soup = BeautifulSoup(html, 'html.parser')  
+                # span 태그에 class 속성 사용 추출
+                review = soup.find_all('span',attrs={'class':'PXMot'})          
+                for item in review:
+                        print(item.get_text())
+                result.append([review])
+                # df['naver_map_review'][i]=review 
+            except: pass
 
-            result.append([review])
-        # except IndexEror: 
-        #     df['naver_map_review'][i]= ''
-        #     print('none') 
-
-
-    # query = df['명칭'].tolist()
-    # wd.get(f'https://map.naver.com/v5/search/{query}=14377346.8495072,4186044.6333144,15,0,0,0,dh')
-    # time.sleep(3)
-    # wd.switch_to.frame('entryIframe')
-    # wd.find_element(By.XPATH,'//*[@id="app-root"]/div/div/div/div[2]/div[1]').click()
-    # time.sleep(3)
-    # html = wd.page_source
-    # soup = BeautifulSoup(html, 'html.parser')
-    # # span 태그에 class 속성 사용 추출하기
-    # review = soup.find_all('span', attrs={'class':'PXMot'})
-    # for item in review:
-    #     print(item.get_text())
-
-    # result.append([review])
+      
 
 
 def main():
     result = []
     print('네이버맵크롤링 : ')
     getNavermap(result)
+    columns = ['review']
+    naver_map_review_df = pd.DataFrame(result,columns=columns)
+    naver_map_review_df.to_csv('./data_tour/리뷰건수.csv',encoding='utf-8')
     print('크롤링끝')
 
 if __name__=='__main__':
